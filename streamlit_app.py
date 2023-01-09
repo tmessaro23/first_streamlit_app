ccffdc2fb3730aced8c877d8a520f737213fb50d
Tara Messaros <122299132+tmessaro23@users.noreply.github.com>
2023-01-09 10:53:30 -0700

    Update streamlit_app.py
    added another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal 🥣')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie 🥗')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg 🐔')
 streamlit.text('🥑🍞 Avocado Toast 🥑🍞')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
